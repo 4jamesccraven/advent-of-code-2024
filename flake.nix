9cdba1c5c06dc1f93901c9578548110aa0f2f2c3
4jamesccraven <4jamesccraven@gmail.com>
2024-12-01 19:42:42 -0500

    initial commit: day 1

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    { flake-utils, nixpkgs, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            libgcc
+            cargo
+            rustc
+          ];
+
+          RUST_SRC_PATH = pkgs.rustPlatform.rustLibSrc;
+          RUST_BACKTRACE = "full";
+
+          shellHook = ''
+            clear; zsh; exit
+          '';
+        };
+      }
+    );
+}

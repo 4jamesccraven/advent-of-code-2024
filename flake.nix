8bb3605c7ab1da8a72475970be44b600e673b615
4jamesccraven <4jamesccraven@gmail.com>
2024-12-09 21:10:14 -0500

    added shell utilities, fix small syntax error in day 8

--- flake.nix
+++ flake.nix
@@ -7,15 +7,39 @@
   };
 
   outputs =
-    { flake-utils, nixpkgs, ... }:
+    { flake-utils, nixpkgs, self }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
         pkgs = import nixpkgs { inherit system; };
       in
       {
+        packages.clean-dirs = pkgs.writeShellScriptBin "clean-dirs" ''
+          #!${pkgs.bash}
+          find ./solutions/ -name 'Cargo.toml' -execdir cargo clean \;
+        '';
+
+        packages.benchmark = pkgs.writeShellScriptBin "benchmark" ''
+          #!${pkgs.bash}
+          echo "day,time"
+          for dir in $(find ./solutions/ -name 'Cargo.toml' -exec dirname {} \; | sort); do
+            day=$(echo "$dir" | sed 's|\(.*\)/day-\([0-9]\+\)|\2|')
+            echo -n "$day"
+
+            real_time=$(cd "$dir" && { time cargo run --release > /dev/null 2>&1; } 2>&1 | grep real| awk '{print $2}')
+
+            minutes=$(echo "$real_time" | awk -F'm' '{print $1}')
+            seconds=$(echo "$real_time" | awk -F'm' '{print $2}' | sed 's/s//')
+
+            printf ",\"%02d:%05.2f\"\n" $minutes $seconds
+          done
+        '';
+
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
+            self.packages.x86_64-linux.clean-dirs
+            self.packages.x86_64-linux.benchmark
+
             libgcc
             cargo
             rustc
